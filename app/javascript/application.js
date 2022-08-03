3c12cd4d7c6413df80967ba0c14b4794fec35c0d
tsumita ryo <kagaki4444@gmail.com>
2022-08-03 11:05:46 +0900

    Bootstrap導入,js反応しないまで

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,6 @@
+//= require jquery3
+//= require popper
+//= require bootstrap
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
